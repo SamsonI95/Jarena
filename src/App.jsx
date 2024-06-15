0bf63d2d70b3f84e6ac91a3a405849b97d172e09
SamsonI95 <samsoniweibo@gmail.com>
2024-06-15 13:24:44 +0100

    Initial commits

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+//App
+import "./App.css";
+
+//Page(s)
+
+//Page Component(s)
+
+
+function App() {
+  return <></>;
+}
+
+export default App;
---
+++ src/config/firebase.jsx
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA5_uZqcGwFtfp6p1swNBsCHpJDCx73vuY",
+  authDomain: "jarena-backend.firebaseapp.com",
+  projectId: "jarena-backend",
+  storageBucket: "jarena-backend.appspot.com",
+  messagingSenderId: "709959270033",
+  appId: "1:709959270033:web:bce468a14c119d860e0930",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
